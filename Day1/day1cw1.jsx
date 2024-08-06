07432373c6285249849bc6d15045654fcabce135
SaparnaKarthikeyan <150768641+SaparnaKarthikeyan@users.noreply.github.com>
2024-08-06 09:19:44 +0530

    Update day1cw1.jsx

--- Day1/day1cw1.jsx
+++ Day1/day1cw1.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-
-function AlertMsg(){
-  alert("Message from Javascript Alert");
-};
-
-export default AlertMsg;
+import react from 'react';
+function AlertMsg()
+{
+    alert("Message from Javascript Alert");
+    console.log("Message to developer");
+}
+function App()
+{
+    return(
+        <div className="App">
+            <h1>Let us see the output of the Javascript</h1>
+            <button onClick={AlertMsg}>Click</button><br></br><br></br>
+        </div>
+    )
+}
+export default App;
